8bbc354ca560c2f4cf646740e06035ecf2ed8aea
Joshua Wong <joshwonghc@gmail.com>
2019-09-29 20:12:07 +0800

    Uncomment old person type methods to reduce errors

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -3,6 +3,7 @@ package seedu.address.model;
 import java.nio.file.Path;
 import java.util.function.Predicate;
 
+import javafx.collections.ObservableList;
 import seedu.address.AlfredException;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.model.entity.Id;
@@ -116,44 +117,44 @@ public interface Model {
 
     Issue deleteIssue(Id id) throws AlfredException;
 
-//    /**
-//     * Replaces address book data with the data in {@code addressBook}.
-//     */
-//    void setAddressBook(ReadOnlyAddressBook addressBook);
-//
-//    /** Returns the AddressBook */
-//    ReadOnlyAddressBook getAddressBook();
-//
-//    /**
-//     * Returns true if a person with the same identity as {@code person} exists in the address book.
-//     */
-//    boolean hasPerson(Person person);
-//
-//    /**
-//     * Deletes the given person.
-//     * The person must exist in the address book.
-//     */
-//    void deletePerson(Person target);
-//
-//    /**
-//     * Adds the given person.
-//     * {@code person} must not already exist in the address book.
-//     */
-//    void addPerson(Person person);
-//
-//    /**
-//     * Replaces the given person {@code target} with {@code editedPerson}.
-//     * {@code target} must exist in the address book.
-//     * The person identity of {@code editedPerson} must not be the same as another existing person in the address book.
-//     */
-//    void setPerson(Person target, Person editedPerson);
-//
-//    /** Returns an unmodifiable view of the filtered person list */
-//    ObservableList<Person> getFilteredPersonList();
-//
-//    /**
-//     * Updates the filter of the filtered person list to filter by the given {@code predicate}.
-//     * @throws NullPointerException if {@code predicate} is null.
-//     */
-//    void updateFilteredPersonList(Predicate<Person> predicate);
+    /**
+     * Replaces address book data with the data in {@code addressBook}.
+     */
+    void setAddressBook(ReadOnlyAddressBook addressBook);
+
+    /** Returns the AddressBook */
+    ReadOnlyAddressBook getAddressBook();
+
+    /**
+     * Returns true if a person with the same identity as {@code person} exists in the address book.
+     */
+    boolean hasPerson(Person person);
+
+    /**
+     * Deletes the given person.
+     * The person must exist in the address book.
+     */
+    void deletePerson(Person target);
+
+    /**
+     * Adds the given person.
+     * {@code person} must not already exist in the address book.
+     */
+    void addPerson(Person person);
+
+    /**
+     * Replaces the given person {@code target} with {@code editedPerson}.
+     * {@code target} must exist in the address book.
+     * The person identity of {@code editedPerson} must not be the same as another existing person in the address book.
+     */
+    void setPerson(Person target, Person editedPerson);
+
+    /** Returns an unmodifiable view of the filtered person list */
+    ObservableList<Person> getFilteredPersonList();
+
+    /**
+     * Updates the filter of the filtered person list to filter by the given {@code predicate}.
+     * @throws NullPointerException if {@code predicate} is null.
+     */
+    void updateFilteredPersonList(Predicate<Person> predicate);
 }
--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -6,8 +6,10 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.logging.Logger;
 
+import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import seedu.address.AlfredException;
 import seedu.address.AlfredRuntimeException;
@@ -360,74 +362,74 @@ public class ModelManager implements Model {
 
     //=========== AddressBook ================================================================================
 
-//    @Override
-//    public void setAddressBook(ReadOnlyAddressBook addressBook) {
-//        this.addressBook.resetData(addressBook);
-//    }
-//
-//    @Override
-//    public ReadOnlyAddressBook getAddressBook() {
-//        return addressBook;
-//    }
-//
-//    @Override
-//    public boolean hasPerson(Person person) {
-//        requireNonNull(person);
-//        return addressBook.hasPerson(person);
-//    }
-//
-//    @Override
-//    public void deletePerson(Person target) {
-//        addressBook.removePerson(target);
-//    }
-//
-//    @Override
-//    public void addPerson(Person person) {
-//        addressBook.addPerson(person);
-//        updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-//    }
-//
-//    @Override
-//    public void setPerson(Person target, Person editedPerson) {
-//        requireAllNonNull(target, editedPerson);
-//
-//        addressBook.setPerson(target, editedPerson);
-//    }
+    @Override
+    public void setAddressBook(ReadOnlyAddressBook addressBook) {
+        this.addressBook.resetData(addressBook);
+    }
+
+    @Override
+    public ReadOnlyAddressBook getAddressBook() {
+        return addressBook;
+    }
+
+    @Override
+    public boolean hasPerson(Person person) {
+        requireNonNull(person);
+        return addressBook.hasPerson(person);
+    }
+
+    @Override
+    public void deletePerson(Person target) {
+        addressBook.removePerson(target);
+    }
+
+    @Override
+    public void addPerson(Person person) {
+        addressBook.addPerson(person);
+        updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+    }
+
+    @Override
+    public void setPerson(Person target, Person editedPerson) {
+        requireAllNonNull(target, editedPerson);
+
+        addressBook.setPerson(target, editedPerson);
+    }
 
 
-    //=========== Filtered Person List Accessors =============================================================
+    // =========== Filtered Person List Accessors =============================================================
 
-//    /**
-//     * Returns an unmodifiable view of the list of {@code Person} backed by the internal list of
-//     * {@code versionedAddressBook}
-//     */
-//    @Override
-//    public ObservableList<Person> getFilteredPersonList() {
-//        return filteredPersons;
-//    }
-//
-//    @Override
-//    public void updateFilteredPersonList(Predicate<Person> predicate) {
-//        requireNonNull(predicate);
-//        filteredPersons.setPredicate(predicate);
-//    }
-//
-//    @Override
-//    public boolean equals(Object obj) {
-//        // short circuit if same object
-//        if (obj == this) {
-//            return true;
-//        }
-//
-//        // instanceof handles nulls
-//        if (!(obj instanceof ModelManager)) {
-//            return false;
-//        }
-//
-//        // state check
-//        ModelManager other = (ModelManager) obj;
-//        return addressBook.equals(other.addressBook)
-//                && userPrefs.equals(other.userPrefs)
-//                && filteredPersons.equals(other.filteredPersons);
-//    }
+    /**
+     * Returns an unmodifiable view of the list of {@code Person} backed by the internal list of
+     * {@code versionedAddressBook}
+     */
+    @Override
+    public ObservableList<Person> getFilteredPersonList() {
+        return filteredPersons;
+    }
+
+    @Override
+    public void updateFilteredPersonList(Predicate<Person> predicate) {
+        requireNonNull(predicate);
+        filteredPersons.setPredicate(predicate);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // short circuit if same object
+        if (obj == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(obj instanceof ModelManager)) {
+            return false;
+        }
+
+        // state check
+        ModelManager other = (ModelManager) obj;
+        return addressBook.equals(other.addressBook)
+                && userPrefs.equals(other.userPrefs)
+                && filteredPersons.equals(other.filteredPersons);
+    }
 }
