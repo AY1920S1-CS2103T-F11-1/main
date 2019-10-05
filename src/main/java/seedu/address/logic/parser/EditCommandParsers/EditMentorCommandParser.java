47f6441f25dbddc56ba2c8b163728ec822850a97
abhiman <abhimanyadav@gmail.com>
2019-10-05 18:55:00 +0800

    Remove redundant files.

--- src/main/java/seedu/address/logic/parser/EditCommandParsers/EditMentorCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParsers/EditMentorCommandParser.java
@@ -5,13 +5,12 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ORGANISATION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_SPECIALISATION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SUBJECT_NAME;
 import seedu.address.logic.commands.editcommand.EditCommand;
 import seedu.address.logic.commands.editcommand.EditMentorCommand;
 import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
-import seedu.address.logic.parser.AlfredParserUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.entity.Id;
 
@@ -29,7 +28,7 @@ public class EditMentorCommandParser implements Parser<EditCommand> {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ORGANISATION,
-                        PREFIX_SPECIALISATION);
+                        PREFIX_SUBJECT_NAME);
         Id id;
 
         return null;
