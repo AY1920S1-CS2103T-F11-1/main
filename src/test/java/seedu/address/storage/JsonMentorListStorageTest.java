d7016137f8f8bb1ea2c7b34be811591ef0e1409d
brianyenna <brianyen.na@gmail.com>
2019-10-10 19:41:26 +0800

    Make JsonTeamListStorageTest and JsonMentorListStorageTest as comprehensive as AB3 tests.

--- src/test/java/seedu/address/storage/JsonMentorListStorageTest.java
+++ src/test/java/seedu/address/storage/JsonMentorListStorageTest.java
@@ -1,21 +1,65 @@
 package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.fail;
+import static seedu.address.testutil.Assert.assertThrows;
 
+import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
+import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.model.entity.Mentor;
 import seedu.address.model.entitylist.MentorList;
 import seedu.address.testutil.TypicalMentors;
 
 class JsonMentorListStorageTest {
+    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonMentorListStorageTest");
+
     @TempDir
     public Path testFolder;
+
+    @Test
+    public void readMentorList_nullFilePath_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> readMentorList(null));
+    }
+
+    private java.util.Optional<MentorList> readMentorList(String filePath) throws Exception {
+        return new JsonMentorListStorage(Paths.get(filePath))
+                .readMentorList(addToTestDataPathIfNotNull(filePath));
+    }
+
+    private Path addToTestDataPathIfNotNull(String prefsFileInTestDataFolder) {
+        return prefsFileInTestDataFolder != null
+                ? TEST_DATA_FOLDER.resolve(prefsFileInTestDataFolder)
+                : null;
+    }
+
+    @Test
+    public void read_missingFile_emptyResult() throws Exception {
+        assertFalse(readMentorList("NonExistentFile.json").isPresent());
+    }
+
+    @Test
+    public void read_notJsonFormat_exceptionThrown() {
+        assertThrows(DataConversionException.class, () -> readMentorList("notJsonFormatMentorList.json"));
+    }
+
+    @Test
+    public void readMentorList_invalidMentorList_throwDataConversionException() {
+        assertThrows(DataConversionException.class, () -> readMentorList("invalidMentorList.json"));
+    }
+
+    @Test
+    public void readMentorList_invalidAndValidMentorList_throwDataConversionException() {
+        assertThrows(DataConversionException.class, () -> readMentorList("invalidAndValidMentorList.json"));
+    }
 
     @org.junit.jupiter.api.Test
     void readAndSaveMentorList_allInOrder_success() throws Exception {
@@ -23,7 +67,7 @@ class JsonMentorListStorageTest {
         MentorList original = TypicalMentors.getTypicalMentorList();
         JsonMentorListStorage mStorage = new JsonMentorListStorage(filePath);
 
-        //Save and read participantList to and from JSON
+        //Save and read mentorList to and from JSON
         mStorage.saveMentorList(original);
         Optional<MentorList> returnedList = mStorage.readMentorList();
 
@@ -33,6 +77,28 @@ class JsonMentorListStorageTest {
             List<Mentor> returnedMentorList = returnedList.get().getSpecificTypedList();
             assertEquals(returnedMentorList, TypicalMentors.getTypicalMentors());
         }
+    }
+
+    @Test
+    public void saveMentorList_nullMentorList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> saveMentorList(null, "SomeFile.json"));
+    }
+
+    /**
+     * Saves {@code mList} at the specified {@code filePath}.
+     */
+    private void saveMentorList(MentorList mList, String filePath) {
+        try {
+            new JsonMentorListStorage(Paths.get(filePath))
+                    .saveMentorList(mList, addToTestDataPathIfNotNull(filePath));
+        } catch (IOException ioe) {
+            throw new AssertionError("There should not be an error writing to the file.", ioe);
+        }
+    }
+
+    @Test
+    public void saveMentorList_nullFilePath_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> saveMentorList(new MentorList(), null));
     }
 
 }
--- src/test/java/seedu/address/storage/JsonTeamListStorageTest.java
+++ src/test/java/seedu/address/storage/JsonTeamListStorageTest.java
@@ -1,22 +1,65 @@
 package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.fail;
+import static seedu.address.testutil.Assert.assertThrows;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
+import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.model.entity.Team;
 import seedu.address.model.entitylist.TeamList;
 import seedu.address.testutil.TypicalTeams;
 
 class JsonTeamListStorageTest {
+    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonTeamListStorageTest");
+
     @TempDir
     public Path testFolder;
+
+    @Test
+    public void readTeamList_nullFilePath_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> readTeamList(null));
+    }
+
+    private java.util.Optional<TeamList> readTeamList(String filePath) throws Exception {
+        return new JsonTeamListStorage(Paths.get(filePath))
+                .readTeamList(addToTestDataPathIfNotNull(filePath));
+    }
+
+    private Path addToTestDataPathIfNotNull(String prefsFileInTestDataFolder) {
+        return prefsFileInTestDataFolder != null
+                ? TEST_DATA_FOLDER.resolve(prefsFileInTestDataFolder)
+                : null;
+    }
+
+    @Test
+    public void read_missingFile_emptyResult() throws Exception {
+        assertFalse(readTeamList("NonExistentFile.json").isPresent());
+    }
+
+    @Test
+    public void read_notJsonFormat_exceptionThrown() {
+        assertThrows(DataConversionException.class, () -> readTeamList("notJsonFormatTeamList.json"));
+    }
+
+    @Test
+    public void readTeamList_invalidTeamList_throwDataConversionException() {
+        assertThrows(DataConversionException.class, () -> readTeamList("invalidTeamList.json"));
+    }
+
+    @Test
+    public void readTeamList_invalidAndValidTeamList_throwDataConversionException() {
+        assertThrows(DataConversionException.class, () -> readTeamList("invalidAndValidTeamList.json"));
+    }
 
     @org.junit.jupiter.api.Test
     void readAndSaveTeamList_allInOrder_success() throws Exception {
@@ -35,5 +78,27 @@ class JsonTeamListStorageTest {
             List<Team> returnedTeamList = returnedList.get().getSpecificTypedList();
             assertEquals(returnedTeamList, TypicalTeams.getTypicalTeams());
         }
+    }
+
+    @Test
+    public void saveTeamList_nullTeamList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> saveTeamList(null, "SomeFile.json"));
+    }
+
+    /**
+     * Saves {@code tList} at the specified {@code filePath}.
+     */
+    private void saveTeamList(TeamList tList, String filePath) {
+        try {
+            new JsonTeamListStorage(Paths.get(filePath))
+                    .saveTeamList(tList, addToTestDataPathIfNotNull(filePath));
+        } catch (IOException ioe) {
+            throw new AssertionError("There should not be an error writing to the file.", ioe);
+        }
+    }
+
+    @Test
+    public void saveTeamList_nullFilePath_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> saveTeamList(new TeamList(), null));
     }
 }
