b919c999017b498da4cb226fc85ba701af067f8e
brianyenna <brianyen.na@gmail.com>
2019-10-10 13:50:10 +0800

    Create and update TypicalMentors, TypicalParticipants and TypicalTeams (these are meant as testing utilities). Basic test for ParticipantListStorage has been created.

--- src/test/java/seedu/address/storage/JsonMentorListStorageTest.java
+++ src/test/java/seedu/address/storage/JsonMentorListStorageTest.java
@@ -1,26 +1,40 @@
 package seedu.address.storage;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.io.TempDir;
+
+import seedu.address.model.entity.Mentor;
+import seedu.address.model.entitylist.MentorList;
+import seedu.address.testutil.TypicalMentors;
+
 class JsonMentorListStorageTest {
+    @TempDir
+    public Path testFolder;
 
     @org.junit.jupiter.api.Test
-    void getMentorListFilePath() {
+    void readAndSaveMentorList_allInOrder_success() throws Exception {
+        Path filePath = testFolder.resolve("TempMentorList.json");
+        MentorList original = TypicalMentors.getTypicalMentorList();
+        JsonMentorListStorage mStorage = new JsonMentorListStorage(filePath);
 
+        //Save and read participantList to and from JSON
+        mStorage.saveMentorList(original);
+        mStorage.saveMentorList(original, Paths.get("data" , "mentorlist.json"));
+        Optional<MentorList> returnedList = mStorage.readMentorList();
+
+        if (returnedList.isEmpty()) {
+            fail("Mentor List read from storage is empty. Optional<MentorList> is empty.");
+        } else {
+            List<Mentor> returnedMentorList = returnedList.get().getSpecificTypedList();
+            assertEquals(returnedMentorList, TypicalMentors.getTypicalMentors());
+        }
     }
-
-    //@org.junit.jupiter.api.Test
-    //void readMentorList() {
-    //}
-    //
-    //@org.junit.jupiter.api.Test
-    //void testReadMentorList() {
-    //}
-    //
-    //@org.junit.jupiter.api.Test
-    //void saveMentorList() {
-    //}
-    //
-    //@org.junit.jupiter.api.Test
-    //void testSaveMentorList() {
-    //}
 
 }
--- src/test/java/seedu/address/storage/JsonParticipantListStorageTest.java
+++ src/test/java/seedu/address/storage/JsonParticipantListStorageTest.java
@@ -3,76 +3,94 @@ package seedu.address.storage;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
-import java.io.IOException;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Optional;
 
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.io.TempDir;
 
-import seedu.address.AlfredException;
-import seedu.address.model.entity.Email;
-import seedu.address.model.entity.Id;
-import seedu.address.model.entity.Name;
 import seedu.address.model.entity.Participant;
-import seedu.address.model.entity.Phone;
-import seedu.address.model.entity.PrefixType;
 import seedu.address.model.entitylist.ParticipantList;
+import seedu.address.testutil.TypicalParticipants;
 
 class JsonParticipantListStorageTest {
+    @TempDir
+    public Path testFolder;
 
     @org.junit.jupiter.api.Test
-    @Disabled
-    void getParticipantListFilePath() {
-        Name n = new Name("p one");
-        Id i = new Id(PrefixType.P, 1);
-        Email e = new Email("p1@gmail.com");
-        Phone p = new Phone("91231233");
-        Participant p1 = new Participant(new Name("p one"),
-                                         new Id(PrefixType.P, 1),
-                                         new Email("p1@gmail.com"),
-                                         new Phone("91231233"));
-        Participant p2 = new Participant(new Name("p two"),
-                new Id(PrefixType.P, 2),
-                new Email("p2@gmail.com"),
-                new Phone("92222222"));
-        Participant p3 = new Participant(new Name("p three"),
-                new Id(PrefixType.P, 3),
-                new Email("p3@gmail.com"),
-                new Phone("93333333"));
+    void readAndSaveParticipantList_allInOrder_success() throws Exception {
+        Path filePath = testFolder.resolve("TempParticipantList.json");
+        ParticipantList original = TypicalParticipants.getTypicalParticipantList();
+        JsonParticipantListStorage pStorage = new JsonParticipantListStorage(filePath);
 
-        ParticipantList pList = new ParticipantList();
-        try {
-            pList.add(p1);
-            pList.add(p2);
-            pList.add(p3);
-        } catch (AlfredException ae) {
-            System.out.println("Oops");
+        //Save and read participantList to and from JSON
+        pStorage.saveParticipantList(original);
+        Optional<ParticipantList> returnedList = pStorage.readParticipantList();
+
+        if (returnedList.isEmpty()) {
+            fail("Participant List read from storage is empty. Optional<ParticipantList> is empty.");
+        } else {
+            List<Participant> returnedParticipantList = returnedList.get().getSpecificTypedList();
+            assertEquals(returnedParticipantList, TypicalParticipants.getTypicalParticipants());
         }
-
-        JsonParticipantListStorage j = new JsonParticipantListStorage(Paths.get("data" , "participantlist.json"));
-        try {
-            j.saveParticipantList(pList);
-        } catch (IOException io) {
-            fail("Problem saving Participant List");
-        }
-
-        try {
-            ParticipantList newPList = j.readParticipantList(Paths.get("data", "participantlist.json")).get();
-            List<Participant> newList = newPList.getSpecificTypedList();
-            List<Participant> origList = pList.getSpecificTypedList();
-            assertEquals(newList.get(0).getEmail(), origList.get(0).getEmail());
-            assertEquals(newList.get(0).getName(), origList.get(0).getName());
-            assertEquals(newList.get(0).getId(), origList.get(0).getId());
-            assertEquals(newList.get(0).getPhone(), origList.get(0).getPhone());
-            assertEquals(newList, origList);
-            //assertEquals(newList, pList.getSpecificTypedList());
-        } catch (Exception newE) {
-            fail("Exception encountered reading ParticipantList: " + newE.getMessage());
-        }
-
-
-
     }
+
+
+
+
+    //
+    //@org.junit.jupiter.api.Test
+    //void getParticipantListFilePath() {
+    //    Name n = new Name("p one");
+    //    Id i = new Id(PrefixType.P, 1);
+    //    Email e = new Email("p1@gmail.com");
+    //    Phone p = new Phone("91231233");
+    //    Participant p1 = new Participant(new Name("p one"),
+    //                                     new Id(PrefixType.P, 1),
+    //                                     new Email("p1@gmail.com"),
+    //                                     new Phone("91231233"));
+    //    Participant p2 = new Participant(new Name("p two"),
+    //            new Id(PrefixType.P, 2),
+    //            new Email("p2@gmail.com"),
+    //            new Phone("92222222"));
+    //    Participant p3 = new Participant(new Name("p three"),
+    //            new Id(PrefixType.P, 3),
+    //            new Email("p3@gmail.com"),
+    //            new Phone("93333333"));
+    //
+    //    ParticipantList pList = new ParticipantList();
+    //    try {
+    //        pList.add(p1);
+    //        pList.add(p2);
+    //        pList.add(p3);
+    //    } catch (AlfredException ae) {
+    //        System.out.println("Oops");
+    //    }
+    //
+    //    JsonParticipantListStorage j = new JsonParticipantListStorage(Paths.get("data" , "participantlist.json"));
+    //    try {
+    //        j.saveParticipantList(pList);
+    //    } catch (IOException io) {
+    //        fail("Problem saving Participant List");
+    //    }
+    //
+    //    try {
+    //        ParticipantList newPList = j.readParticipantList(Paths.get("data", "participantlist.json")).get();
+    //        List<Participant> newList = newPList.getSpecificTypedList();
+    //        List<Participant> origList = pList.getSpecificTypedList();
+    //        assertEquals(newList.get(0).getEmail(), origList.get(0).getEmail());
+    //        assertEquals(newList.get(0).getName(), origList.get(0).getName());
+    //        assertEquals(newList.get(0).getId(), origList.get(0).getId());
+    //        assertEquals(newList.get(0).getPhone(), origList.get(0).getPhone());
+    //        assertEquals(newList, origList);
+    //        //assertEquals(newList, pList.getSpecificTypedList());
+    //    } catch (Exception newE) {
+    //        fail("Exception encountered reading ParticipantList: " + newE.getMessage());
+    //    }
+    //
+    //
+    //
+    //}
 
     //@org.junit.jupiter.api.Test
     //void readParticipantList() {
