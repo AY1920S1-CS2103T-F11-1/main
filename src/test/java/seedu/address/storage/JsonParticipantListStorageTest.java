ec8367d21ede733bb2b1ccc458cb7fb23483d530
brianyenna <brianyen.na@gmail.com>
2019-10-10 18:01:16 +0800

    Update JsonParticipantListStorageTest to be as comprehensive as in AB3 AddressBookStorageTest

--- src/test/java/seedu/address/storage/JsonParticipantListStorageTest.java
+++ src/test/java/seedu/address/storage/JsonParticipantListStorageTest.java
@@ -1,21 +1,65 @@
 package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.fail;
+import static seedu.address.testutil.Assert.assertThrows;
 
+import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
+import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.model.entity.Participant;
 import seedu.address.model.entitylist.ParticipantList;
 import seedu.address.testutil.TypicalParticipants;
 
 class JsonParticipantListStorageTest {
+    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonParticipantListStorageTest");
+
     @TempDir
     public Path testFolder;
+
+    @Test
+    public void readParticipantList_nullFilePath_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> readParticipantList(null));
+    }
+
+    private java.util.Optional<ParticipantList> readParticipantList(String filePath) throws Exception {
+        return new JsonParticipantListStorage(Paths.get(filePath))
+                        .readParticipantList(addToTestDataPathIfNotNull(filePath));
+    }
+
+    private Path addToTestDataPathIfNotNull(String prefsFileInTestDataFolder) {
+        return prefsFileInTestDataFolder != null
+                ? TEST_DATA_FOLDER.resolve(prefsFileInTestDataFolder)
+                : null;
+    }
+
+    @Test
+    public void read_missingFile_emptyResult() throws Exception {
+        assertFalse(readParticipantList("NonExistentFile.json").isPresent());
+    }
+
+    @Test
+    public void read_notJsonFormat_exceptionThrown() {
+        assertThrows(DataConversionException.class, () -> readParticipantList("notJsonFormatParticipantList.json"));
+    }
+
+    @Test
+    public void readParticipantList_invalidParticipantList_throwDataConversionException() {
+        assertThrows(DataConversionException.class, () -> readParticipantList("invalidParticipantList.json"));
+    }
+
+    @Test
+    public void readParticipantList_invalidAndValidParticipantList_throwDataConversionException() {
+        assertThrows(DataConversionException.class, () -> readParticipantList("invalidAndValidParticipantList.json"));
+    }
 
     @org.junit.jupiter.api.Test
     void readAndSaveParticipantList_allInOrder_success() throws Exception {
@@ -35,76 +79,25 @@ class JsonParticipantListStorageTest {
         }
     }
 
+    @Test
+    public void saveParticipantList_nullParticipantList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> saveParticipantList(null, "SomeFile.json"));
+    }
 
+    /**
+     * Saves {@code pList} at the specified {@code filePath}.
+     */
+    private void saveParticipantList(ParticipantList pList, String filePath) {
+        try {
+            new JsonParticipantListStorage(Paths.get(filePath))
+                    .saveParticipantList(pList, addToTestDataPathIfNotNull(filePath));
+        } catch (IOException ioe) {
+            throw new AssertionError("There should not be an error writing to the file.", ioe);
+        }
+    }
 
-
-    //
-    //@org.junit.jupiter.api.Test
-    //void getParticipantListFilePath() {
-    //    Name n = new Name("p one");
-    //    Id i = new Id(PrefixType.P, 1);
-    //    Email e = new Email("p1@gmail.com");
-    //    Phone p = new Phone("91231233");
-    //    Participant p1 = new Participant(new Name("p one"),
-    //                                     new Id(PrefixType.P, 1),
-    //                                     new Email("p1@gmail.com"),
-    //                                     new Phone("91231233"));
-    //    Participant p2 = new Participant(new Name("p two"),
-    //            new Id(PrefixType.P, 2),
-    //            new Email("p2@gmail.com"),
-    //            new Phone("92222222"));
-    //    Participant p3 = new Participant(new Name("p three"),
-    //            new Id(PrefixType.P, 3),
-    //            new Email("p3@gmail.com"),
-    //            new Phone("93333333"));
-    //
-    //    ParticipantList pList = new ParticipantList();
-    //    try {
-    //        pList.add(p1);
-    //        pList.add(p2);
-    //        pList.add(p3);
-    //    } catch (AlfredException ae) {
-    //        System.out.println("Oops");
-    //    }
-    //
-    //    JsonParticipantListStorage j = new JsonParticipantListStorage(Paths.get("data" , "participantlist.json"));
-    //    try {
-    //        j.saveParticipantList(pList);
-    //    } catch (IOException io) {
-    //        fail("Problem saving Participant List");
-    //    }
-    //
-    //    try {
-    //        ParticipantList newPList = j.readParticipantList(Paths.get("data", "participantlist.json")).get();
-    //        List<Participant> newList = newPList.getSpecificTypedList();
-    //        List<Participant> origList = pList.getSpecificTypedList();
-    //        assertEquals(newList.get(0).getEmail(), origList.get(0).getEmail());
-    //        assertEquals(newList.get(0).getName(), origList.get(0).getName());
-    //        assertEquals(newList.get(0).getId(), origList.get(0).getId());
-    //        assertEquals(newList.get(0).getPhone(), origList.get(0).getPhone());
-    //        assertEquals(newList, origList);
-    //        //assertEquals(newList, pList.getSpecificTypedList());
-    //    } catch (Exception newE) {
-    //        fail("Exception encountered reading ParticipantList: " + newE.getMessage());
-    //    }
-    //
-    //
-    //
-    //}
-
-    //@org.junit.jupiter.api.Test
-    //void readParticipantList() {
-    //}
-    //
-    //@org.junit.jupiter.api.Test
-    //void testReadParticipantList() {
-    //}
-    //
-    //@org.junit.jupiter.api.Test
-    //void saveParticipantList() {
-    //}
-    //
-    //@org.junit.jupiter.api.Test
-    //void testSaveParticipantList() {
-    //}
+    @Test
+    public void saveParticipantList_nullFilePath_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> saveParticipantList(new ParticipantList(), null));
+    }
 }
