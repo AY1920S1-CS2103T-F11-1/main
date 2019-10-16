63b103ad1c40b53220ad7520409e1930a9c458dd
Joanna-YJA <joannajoules@gmail.com>
2019-10-16 18:09:43 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/AlfredParser.java
+++ src/main/java/seedu/address/logic/parser/AlfredParser.java
@@ -1,13 +1,13 @@
 package seedu.address.logic.parser;
 
-import java.util.logging.Logger;
-import seedu.address.commons.core.LogsCenter;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
--- src/main/java/seedu/address/ui/entitylistpanel/TeamListPanel.java
+++ src/main/java/seedu/address/ui/entitylistpanel/TeamListPanel.java
@@ -22,7 +22,7 @@ public class TeamListPanel extends EntityListPanel {
 
     public TeamListPanel(ObservableList<Team> teamList) {
         super(FXML);
-            teamListView.setItems(teamList);
+        teamListView.setItems(teamList);
         teamListView.setCellFactory(listView -> new TeamListViewCell());
     }
 
