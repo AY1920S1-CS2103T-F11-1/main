96a761db38c31a57741ee7b87835deb276f2e1b0
abhiman <abhimanyadav@gmail.com>
2019-10-19 23:23:32 +0800

    Standardise view command.

--- src/main/java/seedu/address/logic/parser/viewcommandparser/ViewCommandAllocator.java
+++ src/main/java/seedu/address/logic/parser/viewcommandparser/ViewCommandAllocator.java
@@ -2,6 +2,7 @@ package seedu.address.logic.parser.viewcommandparser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import seedu.address.logic.commands.deletecommand.DeleteCommand;
 import seedu.address.logic.commands.viewcommand.ViewCommand;
 import seedu.address.logic.parser.AlfredParserUtil;
 import seedu.address.logic.parser.CliSyntax;
@@ -14,27 +15,20 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class ViewCommandAllocator implements CommandAllocator<ViewCommand> {
 
-    /**
-     * Parses the ID specified by the user in the view command input to determine
-     * which entity is being viewed, and accordingly calls the appropriate parser to parse
-     * the user input and returns the respective ViewCommand.
-     * @param arg the user input.
-     * @return the appropriate ViewCommand based on which entity is being viewed.
-     * @throws ParseException if the format of the View command is incorrect.
-     */
-    public ViewCommand allocate(String arg) throws ParseException {
+    public ViewCommand allocate(String userInput) throws ParseException {
 
-        String prefix = AlfredParserUtil.getIdPrefix(arg);
+        String entity = AlfredParserUtil.getEntityFromCommand(userInput, ViewCommand.MESSAGE_USAGE);
+        String args = AlfredParserUtil.getArgumentsFromCommand(userInput, ViewCommand.MESSAGE_USAGE);
 
-        switch (prefix) {
-        case CliSyntax.PREFIX_ENTITY_MENTOR:
-            return new ViewMentorCommandParser().parse(arg);
+        switch (entity) {
+        case CliSyntax.ENTITY_MENTOR:
+            return new ViewMentorCommandParser().parse(args);
 
-        case CliSyntax.PREFIX_ENTITY_PARTICIPANT:
-            return new ViewParticipantCommandParser().parse(arg);
+        case CliSyntax.ENTITY_PARTICIPANT:
+            return new ViewParticipantCommandParser().parse(args);
 
-        case CliSyntax.PREFIX_ENTITY_TEAM:
-            return new ViewTeamCommandParser().parse(arg);
+        case CliSyntax.ENTITY_TEAM:
+            return new ViewTeamCommandParser().parse(args);
 
         default:
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
