fbbbe684a5c8f3aef76326a7a114a6cf6e5dcd27
abhiman <abhimanyadav@gmail.com>
2019-10-23 16:58:25 +0800

    Fix typo in error message.

--- src/main/java/seedu/address/logic/commands/GetTopTeamsCommand.java
+++ src/main/java/seedu/address/logic/commands/GetTopTeamsCommand.java
@@ -8,11 +8,10 @@ import seedu.address.model.entity.PrefixType;
 public class GetTopTeamsCommand extends Command {
 
     public static final String COMMAND_WORD = "getTop";
-    private static final String MESSAGE_LEADERBOARD_HEADER = "Current Top %1$s";
     public static final String MESSAGE_SUCCESS = "Showing Current Top %1$s";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": shows the top K teams as the leaderboard stands"
             + " where K is an integer value you type in. \n"
-            + "Format: " + COMMAND_WORD + " K"
+            + "Format: " + COMMAND_WORD + " K \n"
             + "For example: " + COMMAND_WORD + " 5";
 
     private int topTeams;
@@ -26,7 +25,6 @@ public class GetTopTeamsCommand extends Command {
         requireNonNull(model);
         model.getTopK(this.topTeams);
 
-        System.out.println(String.format(MESSAGE_LEADERBOARD_HEADER, topTeams));
         return new CommandResult(String.format(MESSAGE_SUCCESS, topTeams), PrefixType.K);
     }
 
