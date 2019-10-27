ab31f69e8ff329560fbcf31161cdc54a03492f71
Joshua Wong <joshwonghc@gmail.com>
2019-10-27 18:32:33 +0800

    Complete parser tests

--- src/test/java/seedu/address/logic/parser/findcommandparser/FindParticipantCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/findcommandparser/FindParticipantCommandParserTest.java
@@ -3,16 +3,13 @@ package seedu.address.logic.parser.findcommandparser;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import seedu.address.logic.commands.findcommand.FindParticipantCommand;
-import seedu.address.model.entity.Id;
-import seedu.address.model.entity.PrefixType;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
-public class FindParticipantParserTest {
+public class FindParticipantCommandParserTest {
     private FindParticipantCommandParser findParticipantParser = new FindParticipantCommandParser();
-    private Id id = new Id(PrefixType.P, 1);
 
     @Test
     public void parse_validArgs_returnsFindCommand() {
