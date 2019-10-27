939678cdec69421e59d4889e28df7f4563979f73
brianyenna <brianyen.na@gmail.com>
2019-10-27 12:09:18 +0800

    Update ModelHistoryManagerTest.java with tests for latest changes in ModelHistoryManager.java

---
+++ src/test/java/seedu/address/logic/commands/historycommand/RedoCommandTest.java
@@ -0,0 +1,14 @@
+package seedu.address.logic.commands.historycommand;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RedoCommandTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void execute() {
+    }
+}
